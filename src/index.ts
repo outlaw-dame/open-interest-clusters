3d889a38b97cadc6914d05af0cbbcbcf197d1bc4
Damon O <141657791+outlaw-dame@users.noreply.github.com>
2026-04-19 23:29:27 -0400

    Expose graph + community detection layer

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,9 @@ export * from "./loaders/remote-loader.js";
 export * from "./matching/index.js";
 export * from "./matching/resolve-hashtag.js";
 export * from "./matching/match-text.js";
+
+// Graph / Community
+export * from "./graph/cooccurrence-graph.js";
+export * from "./graph/ingest.js";
+export * from "./graph/louvain.js";
+export * from "./graph/community-mapping.js";
