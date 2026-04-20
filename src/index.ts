ea547ecc83b275e7a2fe63283765c67a903570d0
Damon O <141657791+outlaw-dame@users.noreply.github.com>
2026-04-19 21:36:51 -0400

    Expose matching layer in public API

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,8 @@ export * from "./validation/validator.js";
 export * from "./normalization/hashtags.js";
 export * from "./loaders/dataset-loader.js";
 export * from "./loaders/remote-loader.js";
+
+// Matching layer
+export * from "./matching/index.js";
+export * from "./matching/resolve-hashtag.js";
+export * from "./matching/match-text.js";
