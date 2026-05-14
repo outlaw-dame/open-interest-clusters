366bb8e3aa2dae8a699834f4c4a0ada4e6bc812e
Damon O <141657791+outlaw-dame@users.noreply.github.com>
2026-05-14 09:10:06 -0400

    Export ANN deployment config validation

--- src/index.ts
+++ src/index.ts
@@ -28,6 +28,7 @@ export * from "./ann/capability-orchestrator.js";
 export * from "./ann/adaptive-orchestrator.js";
 export * from "./ann/deployment-routing.js";
 export * from "./ann/deployment-profiles.js";
+export * from "./ann/deployment-config.js";
 export * from "./ann/profiles.js";
 export * from "./adapters/pgvector/types.js";
 export * from "./adapters/pgvector/provider.js";
