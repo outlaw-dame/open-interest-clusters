79cc8057687b8167a13ef76f4fdf3c7658fa07fc
Damon O <141657791+outlaw-dame@users.noreply.github.com>
2026-05-14 09:56:49 -0400

    Export ANN config lifecycle helpers

--- src/index.ts
+++ src/index.ts
@@ -29,6 +29,7 @@ export * from "./ann/adaptive-orchestrator.js";
 export * from "./ann/deployment-routing.js";
 export * from "./ann/deployment-profiles.js";
 export * from "./ann/deployment-config.js";
+export * from "./ann/config-lifecycle.js";
 export * from "./ann/profiles.js";
 export * from "./adapters/pgvector/types.js";
 export * from "./adapters/pgvector/provider.js";
