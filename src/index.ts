1b7aacc01011885ef64decf3859339a427e6a66f
Damon O <141657791+outlaw-dame@users.noreply.github.com>
2026-05-15 09:34:24 -0400

    chore: export protocol source context mappers

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,7 @@ export * from "./runtime/hash.js";
 export * from "./recommendation/consent.js";
 export * from "./recommendation/consent-enforcement.js";
 export * from "./recommendation/source-adapter.js";
+export * from "./recommendation/protocol-source-contexts.js";
 export * from "./ann/types.js";
 export * from "./ann/in-memory-ann.js";
 export * from "./ann/serialization.js";
