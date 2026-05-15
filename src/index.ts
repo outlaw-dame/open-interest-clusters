e81e59dfad923c4329f6c4ae15f01b874102f13f
Damon O <141657791+outlaw-dame@users.noreply.github.com>
2026-05-15 14:45:35 -0400

    chore: export interest signal derivation

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,7 @@ export * from "./recommendation/source-adapter.js";
 export * from "./recommendation/protocol-source-contexts.js";
 export * from "./recommendation/consent-gated-source-adapter.js";
 export * from "./recommendation/interest-signal.js";
+export * from "./recommendation/interest-signal-derivation.js";
 export * from "./ann/types.js";
 export * from "./ann/in-memory-ann.js";
 export * from "./ann/serialization.js";
