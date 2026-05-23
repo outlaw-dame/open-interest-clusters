c01e51b885f325e9b9c66388b250b8bdf361c626
Damon O <141657791+outlaw-dame@users.noreply.github.com>
2026-05-23 17:34:47 -0400

    chore: export protocol source normalizers

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,7 @@ export * from "./recommendation/consent.js";
 export * from "./recommendation/consent-enforcement.js";
 export * from "./recommendation/source-adapter.js";
 export * from "./recommendation/protocol-source-contexts.js";
+export * from "./recommendation/protocol-source-normalizers.js";
 export * from "./recommendation/consent-gated-source-adapter.js";
 export * from "./recommendation/interest-signal.js";
 export * from "./recommendation/interest-signal-derivation.js";
