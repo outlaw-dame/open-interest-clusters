7366a3205b404cd5dd99ff93bf0cc5e18d80e3c4
Damon O <141657791+outlaw-dame@users.noreply.github.com>
2026-08-02 09:05:50 -0400

    chore: remove temporary optional-property augmentation

--- src/recommendation/activitypub-curated-account-sets-optional.ts
+++
@@ -1,7 +0,0 @@
-import "./activitypub-curated-account-sets.js";
-
-declare module "./activitypub-curated-account-sets.js" {
-  interface RecommendationCuratedAccountSet {
-    description?: string | undefined;
-  }
-}
